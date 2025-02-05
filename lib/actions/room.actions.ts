4df498adb266fb525971ef0bbab19967d71f1b46
uddugarg <udivyy.99@gmail.com>
2025-02-05 15:05:05 +0530

    refactor: Add Notifications component and use it in Home page

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -83,7 +83,21 @@ export const updateDocumentAccess = async ({ roomId, email, userType, updatedBy 
         });
 
         if (room) {
-            // TODO: send notification to the user
+            const notificationId = nanoid();
+
+            await liveblocks.triggerInboxNotification({
+                userId: email,
+                kind: '$documentAccess',
+                subjectId: notificationId,
+                activityData: {
+                    userType,
+                    title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+                    updatedBy: updatedBy.name,
+                    avatar: updatedBy.avatar,
+                    email: updatedBy.email
+                },
+                roomId
+            })
         }
 
         revalidatePath(`/documents/${roomId}`);
