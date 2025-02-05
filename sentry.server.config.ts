11769e6d9a1c06a6ec58019bfbd48ddef2016472
uddugarg <udivyy.99@gmail.com>
2025-02-05 12:24:59 +0530

    feat: Add Liveblocks configuration and dependencies
    - Added Liveblocks configuration and dependencies to the package.json file.
    - Created the sentry.server.config.ts file to configure Sentry on the server.
    - Created the sentry.edge.config.ts file to configure Sentry for edge features.
    - Added the instrumentation.ts file to handle imports based on the NEXT_RUNTIME environment variable.
    - Added the sentry.client.config.ts file to configure Sentry on the client.
    - Created the app/global-error.tsx file to handle global errors and capture exceptions with Sentry.
    - Created the components/ui/label.tsx file to define the Label component.
    - Created the components/UserTypeSelector.tsx file to define the UserTypeSelector component.
    - Updated the lib/actions/user.action.ts file to include a new function for fetching document users.
    - Updated the app/Provider.tsx file to include the resolveMentionSuggestions function for Liveblocks.
    - Updated the components/CollaborativeRoom.tsx file to include the ShareModal component for sharing the room.

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2c002f6206db6fd9abfe56befe6c32d6@o4507572657127424.ingest.de.sentry.io/4507572659683408",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
